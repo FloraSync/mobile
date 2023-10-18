552c7ba72a7c2cf989a2c135c4553532729fec64
Schuyler Ankele <schuyler.ankele@gmail.com>
2023-10-17 22:00:11 -0700

    Fix missing key pair

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:florasync_mobile/screens/splash_screen.dart';
 import 'package:florasync_mobile/screens/login_screen.dart';
 import 'package:florasync_mobile/screens/home_screen.dart';
 import 'package:florasync_mobile/screens/register_screen.dart';
@@ -8,21 +7,23 @@ import 'package:florasync_mobile/screens/register_screen.dart';
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(FloraSync());
+  runApp(const FloraSync());
   // whenever your initialization is completed, remove the splash screen:
   FlutterNativeSplash.remove();
 }
 
 class FloraSync extends StatelessWidget {
+  const FloraSync({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeScreen(loggedIn: false),
-        '/login': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(loggedIn: true),
-        '/register': (context) => RegisterScreen(),
+        '/': (context) => const HomeScreen(loggedIn: false),
+        '/login': (context) => const LoginScreen(),
+        '/home': (context) => const HomeScreen(loggedIn: true),
+        '/register': (context) => const RegisterScreen(),
       },
     );
   }
